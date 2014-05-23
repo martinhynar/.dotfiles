b4399c57b38d1da62de96b05f35c61a2cc4d407a
Martin Hynar <martin.hynar@gmail.com>
2014-05-23 19:57:23 +0200

    install script improved

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,35 +1,47 @@
 #!/bin/zsh
 
 
-# Initial Checks
-DOTFILES=${HOME}/.dotfiles
-EMACSD=~/.emacs.d
-OH_MY_ZSH=~/.oh-my-zsh
-[ ! -d ${DOTFILES} ] && echo "${DOTFILES} does not exist, exiting" && exit 1
-[ ! -d ${EMACSD} ] && echo "${EMACSD} does not exist, exiting" && exit 1
-[ ! -d ${OH_MY_ZSH} ] && echo "${EMACSD} does not exist, exiting" && exit 1
-
-echo -e "\033[1;33mRefreshing from GitHub\033[0m"
-pushd ${DOTFILES}
-git pull
-popd
-
 makeLink () {
     TARGET_FILE=$1
     SOURCE_FILE="${DOTFILES}${${TARGET_FILE}#${HOME}}"
-    [ ! -f ${TARGET_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 1
     [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 1
-    echo -e "    ${TARGET_FILE} \033[1;33m->\033[0m ${SOURCE_FILE}"
+    echo -e "\t${TARGET_FILE} -> ${SOURCE_FILE}"
     rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
 
 }
 
 
-echo -e "\033[1;33mSetting up Emacs configuration\033[0m"
+
+# Initial Checks
+DOTFILES=${HOME}/.dotfiles
+EMACSD=~/.emacs.d
+OH_MY_ZSH=~/.oh-my-zsh
+[ ! -d ${DOTFILES} ] && echo "${DOTFILES} does not exist, exiting" && exit 1
+
+echo -n "Refreshing .dotFiles from GitHub: "
+git -C ${DOTFILES} pull
+
+# OH MY ZSH
+TARGET=${OH_MY_ZSH}
+[ -d ${TARGET} ] && echo -en "Updating Oh My ZSH: " && git -C ${TARGET} pull
+[ ! -d ${TARGET} ] && echo "Installing Oh My ZSH" && mkdir -p ${TARGET} && git -C ${TARGET} clone git@github.com:robbyrussell/oh-my-zsh.git .
+
+echo "Setting up Zsh and Oh-My-Zsh custom additions"
+makeLink ${HOME}/.zshrc
+mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-company
+mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-settings
+makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
+makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
+
+
+
+
+# EMACS
+echo "Setting up Emacs configuration"
+mkdir -p ${EMACSD}
 makeLink ${EMACSD}/init.el
 
-echo -e "\033[1;33mSetting up Zsh and Oh-My-Zsh custom additions\033[0m"
-makeLink ${HOME}/.zshrc
-makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
-makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-company/oh-my-company.plugin.zsh
-
+# YASNIPPET
+TARGET=${EMACSD}/git-packages/yasnippet
+[ -d ${TARGET} ] && echo -en "Updating Emacs YASnippet package: " && git -C ${TARGET} pull
+[ ! -d ${TARGET} ] && echo "Installing Emacs YASnippet package" && mkdir -p ${TARGET} && git -C ${TARGET} clone --recursive git@github.com:capitaomorte/yasnippet.git .
