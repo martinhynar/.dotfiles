4311312780f13665fe87d48e4d2f00d2b341cc47
martinhynar <martin.hynar@gmail.com>
2014-07-26 10:20:14 +0200

    Update yum-install.sh

--- bin/yum-install.sh
+++ bin/yum-install.sh
@@ -3,5 +3,6 @@
 PACKAGES=()
 PACKAGES=(${PACKAGES[@]} emacs)
 PACKAGES=(${PACKAGES[@]} emacs-auto-complete)
-
+PACKAGES=(${PACKAGES[@]} R)
+PACKAGES=(${PACKAGES[@]} gnumeric)
 sudo yum -y install ${PACKAGES[@]}
