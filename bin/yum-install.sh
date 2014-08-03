02a61a7f044c2633e58050082cb29e9a31cd72a8
martinhynar <martin.hynar@gmail.com>
2014-08-03 13:41:27 +0200

    Update yum-install.sh

--- bin/yum-install.sh
+++ bin/yum-install.sh
@@ -5,4 +5,6 @@ PACKAGES=(${PACKAGES[@]} emacs)
 PACKAGES=(${PACKAGES[@]} emacs-auto-complete)
 PACKAGES=(${PACKAGES[@]} R)
 PACKAGES=(${PACKAGES[@]} gnumeric)
+PACKAGES=(${PACKAGES[@]} xrandr xbacklight)
+
 sudo yum -y install ${PACKAGES[@]}
