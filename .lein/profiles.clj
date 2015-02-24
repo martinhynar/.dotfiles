5881cff4ceb6b3aa18ca3642ad3f95cf14a8c087
martinhynar <martin.hynar@gmail.com>
2015-02-24 17:44:05 +0100

    lein-localrepo

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [
                   [venantius/ultra "0.2.1"]
                   [lein-ancient "0.6.1"]
+                  [lein-localrepo "0.5.3"]
                   ]
         :ultra {:color-scheme :solarized_dark
                 :repl         true
