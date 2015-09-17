749b58de7d77002870d20b64bdbc6764ecea663a
Martin Hynar <mhynar@netsuite.com>
2015-09-17 10:57:34 +0200

    incorrect check fixed

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@
 makeLink () {
     TARGET_FILE=$1
     SOURCE_FILE="${DOTFILES}${${TARGET_FILE}#${HOME}}"
-    [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && SOURCE_FILE="${DOTFILESCOMP}${${TARGET_FILE}#${HOME}}" && [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 1
+    [ ! -f ${SOURCE_FILE} ] && echo "File ${SOURCE_FILE} does not exist!" && SOURCE_FILE="${DOTFILESCOMP}${${TARGET_FILE}#${HOME}}" && [ ! -f ${SOURCE_FILE} ] && echo "File ${TARGET_FILE} does not exist!" && exit 0
     echo -e "\t${TARGET_FILE} -> ${SOURCE_FILE}"
     rm -f ${TARGET_FILE} && ln -s ${SOURCE_FILE} ${TARGET_FILE}
 
@@ -32,8 +32,10 @@ makeLink ${HOME}/.zshrc
 mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-company
 mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-settings
 mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-xfce
+mkdir -p ${OH_MY_ZSH}/custom/plugins/oh-my-elasticsearch
 makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-settings/oh-my-settings.plugin.zsh
 makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-xfce/oh-my-xfce.plugin.zsh
+makeLink ${OH_MY_ZSH}/custom/plugins/oh-my-elasticsearch/oh-my-elasticsearch.plugin.zsh
 
 
 
