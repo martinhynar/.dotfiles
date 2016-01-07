78685e5db75c879abea1e43acb868388a5124cce
martinhynar <martin.hynar@gmail.com>
2016-01-07 10:17:04 +0100

    make sure additional packages are installed

--- bin/yum-install.sh
+++ bin/yum-install.sh
@@ -6,5 +6,8 @@ PACKAGES=(${PACKAGES[@]} emacs-auto-complete)
 PACKAGES=(${PACKAGES[@]} R)
 PACKAGES=(${PACKAGES[@]} gnumeric)
 PACKAGES=(${PACKAGES[@]} xrandr xbacklight)
+PACKAGES=(${PACKAGES[@]} jq)
+PACKAGES=(${PACKAGES[@]} xclip)
+PACKAGES=(${PACKAGES[@]} git)
 
 sudo yum -y install ${PACKAGES[@]}
