b4997d10071726e5e3338d3d3bf3d7a692c996bb
Martin Hynar <mhynar@netsuite.com>
2016-01-08 14:29:14 +0100

    es versions alias

--- .oh-my-zsh/custom/plugins/oh-my-elasticsearch/oh-my-elasticsearch.plugin.zsh
+++ .oh-my-zsh/custom/plugins/oh-my-elasticsearch/oh-my-elasticsearch.plugin.zsh
@@ -2,3 +2,4 @@
 
 # ALIASES
 alias ,es_disk_usage_per_node="curl -s -XGET \"http://${ESHOST}:${ESPORT}/_cat/nodes?v&h=disk.avail,name\" | sort"
+alias ,es_version_node="curl -s -XGET \"http://${ESHOST}:${ESPORT}/_cat/nodes?v&h=version,name\" | sort"
